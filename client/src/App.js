44622af49ca721d42d5e0c8fa0317fad28282989
Mihai-Adrian Andrei <mihai.andrei1597@gmail.com>
2020-03-04 16:40:59 +0200

    added ImagesInfiniteScroll component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import ImagesInfiniteScroll from './components/ImagesInfiniteScroll';
 import './App.css';
 
+const alias = "unsplash-downloader-";
 const App = () => {
   return (
-    <div className="App">
+    <div className={`${alias}wrapper`}>
       App
+      <ImagesInfiniteScroll />
     </div>
   );
 }
