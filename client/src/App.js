33f5540f4cc8f0ab0bf181929f5d352ccbd8cf78
Mihai-Adrian Andrei <mihai.andrei1597@gmail.com>
2020-03-05 17:02:34 +0200

    added header/scrollbutton + search functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ImagesInfiniteScroll from './components/ImagesInfiniteScroll';
 import './App.css';
+import Header from './components/Header';
+import ScrollButton from './components/ScrollButton';
 
 const alias = "unsplash-downloader-";
+
 const App = () => {
+  const [ searchValue, setSearchValue ] = React.useState('');
   return (
     <div className={`${alias}wrapper`}>
-      App
-      <ImagesInfiniteScroll />
+      <Header searchValue={ searchValue } setSearchValue={ setSearchValue } />
+      <ImagesInfiniteScroll searchValue={ searchValue }/>
+      <ScrollButton />
     </div>
   );
 }
