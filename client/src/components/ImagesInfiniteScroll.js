054c7fc628dcb782961da035055d3ce2197114e8
Mihai-Adrian Andrei <mihai.andrei1597@gmail.com>
2020-03-05 17:02:17 +0200

    added search functionality

--- client/src/components/ImagesInfiniteScroll.js
+++ client/src/components/ImagesInfiniteScroll.js
@@ -1,52 +1,102 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from 'react-loader-spinner'
+import Image from './Image';
 
 const alias = "unsplash-downloader-";
 
-const ImagesInfiniteScroll = () => {
-  const [items, setItems] = useState([]);
+const ImagesInfiniteScroll = ( { searchValue } ) => {
+  const [defaultItems, setDefaultItems] = useState([]);
   const [paginate, setPaginate] = useState({
     start: 1,
     count: 30
   });
-  
+  const [searchedItems, setSearchedItems] = useState([]);
+  const [paginateSearchedItems, setPaginateSearchedItems] = useState({
+    start: 1,
+    count: 30
+  });
+
   useEffect(() => {
     axios.get(`/api/photos?start=${paginate.start}&count=${paginate.count}`)
       .then((res) => {
-        
-        setItems([...items, ...res.data]);
+        setDefaultItems(defaultItems => [...defaultItems, ...res.data]);
       })
   }, [paginate])
 
-  const fetchImages = () => {
-    setPaginate({
-      count: paginate.count,
-      start: paginate.start + 1
-    })
+  useEffect(() => {
+    setSearchedItems([]);
+    setPaginateSearchedItems(paginateSearchedItems => ({
+      ...paginateSearchedItems,
+      start: 1
+    }));
+  }, [searchValue])
+
+  useEffect(() => {
+    if( searchValue !== '') {
+      axios.get(`/api/photos/search?start=${paginateSearchedItems.start}&count=${paginateSearchedItems.count}&searchValue=${searchValue}`)
+      .then((res) => {
+        console.log(res.data.results);
+        setSearchedItems(searchedItems => [...searchedItems, ...res.data.results]);
+      })
+    }
+    
+  }, [paginateSearchedItems])
+
+
+
+  const increasePagination = (type) => {
+    if(type === 'default') {
+      setPaginate({
+        count: paginate.count,
+        start: paginate.start + 1
+      })
+    } else if( type === 'search') {
+      setPaginateSearchedItems({
+        count: paginateSearchedItems.count,
+        start: paginateSearchedItems.start + 1
+      })
+    }
+    
   }
 
-  const buildInfiniteScroll = () => {
-
-    return <InfiniteScroll
-      dataLength={ items.length }
-      className={`${alias}images`}
-      // next={ fetchImages }
-      hasMore={ true }
-      loader={<h4 className={`${alias}loading`}>Loading...</h4>}
-    >
-      {
-        items.map((image, index) => {
-          return <div key={ index } className={`${alias}gallery-item`}><img key={ index } className={`${alias}single-photo`} src={ image.urls.small } alt="" /></div>
-        })
-      }
-    </InfiniteScroll> 
+  const buildLoading = () => {
+    return <div className={`${alias}loading-wrapper`}>
+      <Loader
+        type="Puff"
+        color="#fff"
+        height={100}
+        width={100}
+        style={{textAlign: 'center'}}
+      />
+      <p style={{color: '#fff'}}>Loading Images</p>
+    </div>
   }
 
-  console.log(items);
+  const buildInfiniteScroll = (items, type) => {
+
+    return (
+      <InfiniteScroll
+        dataLength={ items.length }
+        className={`${alias}images`}
+        next={ () => increasePagination(type) }
+        hasMore={ true }
+        loader={ buildLoading() }>
+        {
+          items.map((image, index) => {
+            return <Image  key={ index }  image={image} />
+          })
+        }
+      </InfiniteScroll> 
+    )
+  }
+
+
   return (
     <div className={`${alias}infinite-scroll-wrapper`}> 
-      { buildInfiniteScroll() }
+      { searchValue === '' && searchedItems.length === 0 ? buildInfiniteScroll(defaultItems, 'default') : buildInfiniteScroll(searchedItems, 'search')}
     </div>
   )
 }
